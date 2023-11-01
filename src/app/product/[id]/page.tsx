6f94682ec5ac65772cde8c6f6e961d7936398657
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-11-02 00:06:13 +0500

    products page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,5 @@
 import CustomImage from '@/components/CustomImage';
 import { notFound } from 'next/navigation';
-import React from 'react';
 
 interface Props {
 	params: {
@@ -12,6 +11,7 @@ const ProductDetailedPage = async ({ params: { id } }: Props) => {
 	try {
 		const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 		const product = await res.json();
+
 		return (
 			<div className='max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10'>
 				<CustomImage product={product} />
@@ -21,6 +21,7 @@ const ProductDetailedPage = async ({ params: { id } }: Props) => {
 						<h1 className='text-2xl md:text-4xl font-bold'>{product.title}</h1>
 						<h2 className='text-gray-500 font-bold text-xl md:text-3xl'>${product.price}</h2>
 					</div>
+
 					<div>
 						<p className='text-xs md:text-sm'>{product.description}</p>
 					</div>
